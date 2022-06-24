6582042f812f9a9a1ab83f092ad28ecb6c565b75
AlexanderBlackman <51733876+AlexanderBlackman@users.noreply.github.com>
2022-06-24 11:56:27 +1000

    Added file type associations,  it now can open jpg/png/jpeg files using 'Open with'

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,26 +1,15 @@
 ﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Imaging;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Storage.Pickers;
-using System.Runtime.InteropServices;
-using WinRT;
-using WinUIEx;
-using Windows.Storage;
-using Windows.Graphics.Imaging;
-using Windows.Storage.Streams;
 using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using WinUIEx;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -38,6 +27,8 @@ namespace ImageOverlay
         double ratio;
         public double WindowWidth { get; set; }
         public double WindowHeight { get; set; }
+        Point pointerPosition;
+
 
         //WindowEx rootWindow = (WindowEx)Window.Current;
         public MainWindow()
@@ -46,11 +37,15 @@ namespace ImageOverlay
             // this.SizeChanged += MainWindow_SizeChanged; 
             string[] arguments = Environment.GetCommandLineArgs();
             this.IsTitleBarVisible = false;
-            this.ExtendsContentIntoTitleBar = true;
-            SetTitleBar(toolbarGrid);
+            //this.ExtendsContentIntoTitleBar = true;
+
+
+            //SetTitleBar(cheekyTitlebarspace);
+            //  SetTitleBar(toolbarGrid);
             this.Title = "ImageOverlay";
 
             LoadImageOnLaunch();
+            //this.IsMinimizable = false;
 
 
 
@@ -174,54 +169,41 @@ namespace ImageOverlay
             this.Close();
         }
 
+        private void selectedImage_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            //Get the position of the pointer relative to the image.
+            //  pointerPosition = e.GetCurrentPoint(selectedImage).Position;
 
 
 
+        }
+
+        private void selectedImage_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            //pointerPosition = e.GetCurrentPoint(selectedImage).Position;
+            ////move image to pointer position
+            //this.CenterOnPoint (pointerPosition._x, pointerPosition._y);
+
+        }
+
+        private void imageGrid_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+
+        }
+
+        private void imageGrid_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            //Get the position of the mouse
+            //pointerPosition = e.GetCurrentPoint(imageGrid).Position;
+
+            // move image to place the pointer was released
+            //this.CenterOnPoint(pointerPosition._x, pointerPosition._y);
 
 
 
-
-
-        //IDEA  See what happens when you set the Drag event 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //[DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto, PreserveSig = true, SetLastError = false)]
-        //public static extern IntPtr GetActiveWindow();
-
-        //public MainWindow()
-        //{
-        //    this.InitializeComponent();
-
-        //   Image image = new Image() { Source = new BitmapImage(new Uri("b://motivate3.jpg")) };
-        //   // TapableImage image = new TapableImage();
-
-
-        //    //TitleBar = image;
-
-        //    this.ExtendsContentIntoTitleBar = true;
-        //    this.IsAlwaysOnTop = true;
-        //    SetTitleBar(imageGrid);
-        // this.SizeChanged += MainWindow_SizeChanged; 
-        //    string[] arguments = Environment.GetCommandLineArgs();
-        //  LoadImageOnLaunch();
-
-
-
-        // OpenFileWindow();
-
-
-
+            //pointerPosition = e.GetCurrentPoint(imageGrid).Position;
+            //this.CenterOnPoint(pointerPosition._x, pointerPosition._y);
+            //this.CenterOnPoint(1900, 1400);
+        }
     }
 }
